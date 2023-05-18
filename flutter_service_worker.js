d29c3a090c24e1ae65705e62af890265f7813516
jeffzheng13 <jeffreyzheng12@gmail.com>
2023-05-18 23:39:13 +0000

    Created AboutMe Page and added functionality

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,45 +2,51 @@
 const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
-const RESOURCES = {
-  "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"index.html": "9c6709bfa8c80dcf2ad4dc60f39d193a",
-"/": "9c6709bfa8c80dcf2ad4dc60f39d193a",
-"main.dart.js": "5548e6825407629044a80fe89aa75f39",
-"manifest.json": "493757defcfefaf370ef90ea0954c227",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"assets/FontManifest.json": "3ffcc53c9188f183258a3785eefc49c1",
-"assets/NOTICES": "43d69f7919e4b89e60b666084f5894b3",
-"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/shaders/ink_sparkle.frag": "ab4751ef630837e294889ca64470bb70",
-"assets/assets/DALL%25C2%25B7E%25202023-01-21%252016.25.42%2520-%2520clipart%2520image%2520of%2520a%2520pill%2520with%2520technology%2520background.png": "eee3421935d7ea9ad61164da48cdb4e8",
-"assets/assets/b_frank_w_me.JPG": "7cd8b3d2cda84fc6c8cb3f39b7bfef0f",
+
+const RESOURCES = {"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
+"canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
+"canvaskit/canvaskit.wasm": "f48eaf57cada79163ec6dec7929486ea",
+"canvaskit/skwasm.worker.js": "19659053a277272607529ef87acf9d8a",
+"canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
+"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
+"canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
+"canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
+"assets/assets/DALL%25C2%25B7E%25202023-01-21%252016.26.30%2520-%2520clipart%2520image%2520of%2520a%2520red%2520and%2520white%2520pill%2520with%2520technology%2520background.png": "44fea540ed334f8e3b980b3aa44c1094",
 "assets/assets/stemgearlogo.png": "71a355c37871e50a8d1137837a475f86",
 "assets/assets/fonts/Unbounded-Regular.ttf": "2430b9e2222630e5794de6de534e7a68",
+"assets/assets/DALL%25C2%25B7E%25202023-01-21%252016.25.42%2520-%2520clipart%2520image%2520of%2520a%2520pill%2520with%2520technology%2520background.png": "eee3421935d7ea9ad61164da48cdb4e8",
+"assets/assets/lostatpittscreenshot.jpg": "16bf8883f4138c55ac3ba3317e8942a9",
+"assets/assets/DALLE%25202023-01-21%252016.25.26%2520-%2520clipart%2520image%2520of%2520a%2520pill%2520with%2520technology%2520background.png": "5037321e206db30adb948863ac16cf1c",
+"assets/assets/running.jpg": "2afa635fd552cb539060d32b0182abcc",
+"assets/assets/b_frank_w_me.JPG": "7cd8b3d2cda84fc6c8cb3f39b7bfef0f",
+"assets/assets/bingsuimage.jpg": "666cc230523a357ce4d668e7ad45120b",
 "assets/assets/schedule.jpg": "fe427f58f6f16f7688ac1897466e5295",
 "assets/assets/DALL%25C2%25B7E%25202023-01-21%252016.25.48%2520-%2520clipart%2520image%2520of%2520a%2520pill%2520with%2520technology%2520background.png": "35a5b85b9bb8723f2638fcc44c585d67",
-"assets/assets/DALL%25C2%25B7E%25202023-01-21%252016.26.30%2520-%2520clipart%2520image%2520of%2520a%2520red%2520and%2520white%2520pill%2520with%2520technology%2520background.png": "44fea540ed334f8e3b980b3aa44c1094",
-"assets/assets/DALLE%25202023-01-21%252016.25.26%2520-%2520clipart%2520image%2520of%2520a%2520pill%2520with%2520technology%2520background.png": "5037321e206db30adb948863ac16cf1c",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/AssetManifest.json": "ef4bdbee9ecc2d726ad4ddc72a776ced",
-"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
-"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
-"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
-"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
-"version.json": "03acefc4795e8573b194262cd3a4419f"
-};
-
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"assets/fonts/MaterialIcons-Regular.otf": "62ec8220af1fb03e1c20cfa38781e17e",
+"assets/NOTICES": "5e176a066a54fa5cdb4697275cef12a8",
+"assets/packages/ionicons/assets/fonts/Ionicons.ttf": "757f33cf07178f986e73b03f8c195bd6",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
+"assets/FontManifest.json": "8a8ff6efe6647822b7d96cf5744550ff",
+"assets/AssetManifest.json": "1a15fe03e1de115deba54be1187322eb",
+"assets/AssetManifest.smcbin": "2e8188deba2c43d778894bce69c387b2",
+"index.html": "b13c30af19a6430d56d550cda0c1f7ff",
+"/": "b13c30af19a6430d56d550cda0c1f7ff",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"version.json": "03acefc4795e8573b194262cd3a4419f",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"main.dart.js": "be3380d15fe459d2a301786ab702db45",
+"manifest.json": "493757defcfefaf370ef90ea0954c227"};
 // The application shell files that are downloaded before a service worker can
 // start.
-const CORE = [
-  "main.dart.js",
+const CORE = ["main.dart.js",
 "index.html",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
+
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
   self.skipWaiting();
@@ -51,7 +57,6 @@ self.addEventListener("install", (event) => {
     })
   );
 });
-
 // During activate, the cache is populated with the temp files downloaded in
 // install. If this service worker is upgrading from one with a saved
 // MANIFEST, then use this to retain unchanged resource files.
@@ -73,6 +78,8 @@ self.addEventListener("activate", function(event) {
         await caches.delete(TEMP);
         // Save the manifest to make future upgrades efficient.
         await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
         return;
       }
       var oldManifest = await manifest.json();
@@ -98,6 +105,8 @@ self.addEventListener("activate", function(event) {
       await caches.delete(TEMP);
       // Save the manifest to make future upgrades efficient.
       await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
       return;
     } catch (err) {
       // On an unhandled exception the state of the cache cannot be guaranteed.
@@ -108,7 +117,6 @@ self.addEventListener("activate", function(event) {
     }
   }());
 });
-
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
@@ -148,7 +156,6 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
 self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
@@ -161,7 +168,6 @@ self.addEventListener('message', (event) => {
     return;
   }
 });
-
 // Download offline will check the RESOURCES for all files not in the cache
 // and populate them.
 async function downloadOffline() {
@@ -182,7 +188,6 @@ async function downloadOffline() {
   }
   return contentCache.addAll(resources);
 }
-
 // Attempt to download the resource online before falling back to
 // the offline cache.
 function onlineFirst(event) {
